01050adeb30bae3340441fa5d6d45c36603e08fc
mukul anand <MukulAnand@MUKULs-MacBook-Pro.local>
2024-03-21 01:56:55 +0530

    codeBees: remove hashrouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {  RouterProvider, createHashRouter} from "react-router-dom";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import UserList from "./UserList/UserList";
 import UserDetail from "./UserDetail/UserDetail";
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/*",
     element: <App />,
@@ -22,10 +22,7 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
   </React.StrictMode>
-
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
